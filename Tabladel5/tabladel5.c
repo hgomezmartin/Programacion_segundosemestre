be6f1608b30d74635636068d3952a6bf279f4e0b
hgomezmartin <hugogmartin@hotmail.com>
2022-02-21 19:23:14 +0100

    inicialización

---
+++ Tabladel5/tabladel5.c
@@ -0,0 +1,41 @@
+/** Nombre del Programa: tablaDeMultiplicarDel5.c
+* Descripción: Ejercicio de Prácticas. El programa muestra por pantalla la tabla de multiplicar del 5
+
+* Organización: UBU, Grado en Ing. Informática 2020/21, Programación
+
+* Autor: Hugo Gómez Martín
+
+* Fecha: 16/02/2022
+
+* Versión: 1.0v
+
+* Lista de versiones:   - 1.0v
+
+*/
+
+
+// Directivas para el procesador 
+
+#include <stdio.h>
+
+// Función principal 
+
+int main(){
+    printf(0*5=0);
+    printf(1*5=5);
+    printf(2*5=10);
+    printf(3*5=15);
+    printf(4*5=20);
+    printf(5*5=25);
+    printf(6*5=30);
+    printf(7*5=35);
+    printf(8*5=40);
+    printf(9*5=45);
+    printf(10*5=50);
+
+    return 0;       
+}
+
+//final del programa
+
+
---
+++ triangulo/triangulo.c
@@ -0,0 +1,30 @@
+/** Nombre del Programa: triangulo.c
+* Descripción: Ejercicio de Prácticas. El programa muestra por pantalla un Triángulo rectángulo
+
+* Organización: UBU, Grado en Ing. Informática 2020/21, Programación
+
+* Autor: Hugo Gómez Martín
+
+* Fecha: 21/02/2022
+
+* Versión: 1.0v
+
+* Lista de versiones:   - 1.0v
+
+*/
+
+
+//Directivas preprocesador
+
+#include <stdio.h>
+
+//funcion principal
+int main(){
+    printf("triangulo\n");
+    printf("\t*\n"
+          "\t* *\n"
+         "\t*   *\n"
+         "\t******\n");
+    return 0;
+}
+
